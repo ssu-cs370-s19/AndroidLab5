cba7c729cfa7db7840dda16f7c7a5406a0bcc7fb
student <student@ssu-cs-dar28-167-223.sonoma.edu>
2019-03-11 19:36:26 -0700

    working code complete

--- app/src/main/java/ssu/softwarednd/spring19/androidlab5/RecipeViewAdapter.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab5/RecipeViewAdapter.java
@@ -2,6 +2,7 @@ package ssu.softwarednd.spring19.androidlab5;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import ssu.softwarednd.spring19.androidlab5.models.RecipeModel;
 
 
 public class RecipeViewAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
+    private static final String TAG = "RecipeViewAdapter";
 
     private List<RecipeModel> recipeCollection;
 
@@ -22,6 +24,7 @@ public class RecipeViewAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
         View inflatedView = LayoutInflater.from(parent.getContext())
             .inflate(R.layout.recycler_list_recipe_item, parent, false);
         return new RecipeViewHolder(inflatedView);
@@ -29,12 +32,14 @@ public class RecipeViewAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull RecipeViewHolder recipeViewHolder, int position) {
-
+        Log.d(TAG, "onBindViewHolder");
+        RecipeModel model = recipeCollection.get(position);
+        recipeViewHolder.bindView(model);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return recipeCollection.size();
     }
 
 }
