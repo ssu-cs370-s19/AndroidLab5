316ce3e8a8810f3a5a826a2a8f34c5b42acf9545
student <student@ssu-cs-dar28-167-203.sonoma.edu>
2019-03-11 18:55:56 -0700

    part 1 completed

--- app/src/main/java/ssu/softwarednd/spring19/androidlab5/models/RecipeResponse.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab5/models/RecipeResponse.java
@@ -1,14 +1,16 @@
 
 package ssu.softwarednd.spring19.androidlab5.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class RecipeResponse {
 
     private int totalMatchCount;
 
+    @SerializedName("matches")
     private List<RecipeModel> recipes;
-
 
     public int getTotalMatchCount() {
         return totalMatchCount;
--- app/src/main/java/ssu/softwarednd/spring19/androidlab5/utility/RecipeParser.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab5/utility/RecipeParser.java
@@ -1,14 +1,16 @@
 package ssu.softwarednd.spring19.androidlab5.utility;
 
+import com.google.gson.Gson;
+
 import java.util.List;
 
 import ssu.softwarednd.spring19.androidlab5.models.RecipeModel;
+import ssu.softwarednd.spring19.androidlab5.models.RecipeResponse;
 
 public class RecipeParser {
 
     // TAG is used in logging, to help trace where output comes from
     private static final String TAG = "RecipeParser";
-
 
     /**
      * Returns a list of RecipeModels, representing recipes in the json
@@ -16,11 +18,17 @@ public class RecipeParser {
      * @param json String json representing the server's response
      * @return List of recipes (as RecipeModels)
      */
+
     public static List<RecipeModel> getMatches(String json) {
 
-        // parse `json`, return a list of recipes
+        //create an instance of Gson
+        Gson gson = new Gson();
 
-        return null;
+        //use Gson to inflate a RecipeReponse
+        RecipeResponse response = gson.fromJson(json, RecipeResponse.class);
+        List<RecipeModel> list = response.getRecipes();
+
+        return list;
     }
 
 }
